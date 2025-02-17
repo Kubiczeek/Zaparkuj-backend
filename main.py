52dc6ee3f601ce7372493c6a6ca332ec5b38a051
Kubiczeek <jakubakubek@gmail.com>
2025-02-17 20:47:03 +0100

    Add CORS middleware and update parking occupancy endpoint

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.middleware.cors import CORSMiddleware
 
 from lib.vehicleDetection import count_vehicle
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 security = HTTPBasic()
 
@@ -41,13 +52,16 @@ def get_newest_file(folder):
 async def root():
     return JSONResponse(content=jsonable_encoder({"message": "Welcome to the parking occupancy API"}), status_code=status.HTTP_200_OK)
 
-@app.get("/api/v1/parking-occupancy/{directory}")
-async def say_hello(directory: str, credentials: Annotated[HTTPBasicCredentials, Depends(is_authorized)], conf: float = 0.3):
+@app.get("/api/v1/parking-occupancy/{id}")
+async def say_hello(id: int, credentials: Annotated[HTTPBasicCredentials, Depends(is_authorized)], conf: float = 0.3):
+    directory = f"parking-lot-{id}"
     if not os.path.exists(f"{PARKING_LOTS_DIR}/{directory}"):
         return JSONResponse(content=jsonable_encoder({"error": "Parking lot not found"}), status_code=status.HTTP_404_NOT_FOUND)
     if conf < 0 or conf > 1:
         return JSONResponse(content=jsonable_encoder({"error": "Confidence threshold must be between 0 and 1"}), status_code=status.HTTP_400_BAD_REQUEST)
     image = get_newest_file(directory)
     count = count_vehicle(f"{PARKING_LOTS_DIR}/{directory}/{image}", conf)
+    # if (id == 1):
+    #    count = 90
     time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return JSONResponse(content=jsonable_encoder({"vehicle_count": count, "conf": conf, "time_stamp": time_stamp}), status_code=status.HTTP_200_OK)
